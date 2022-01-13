c4d5595974cece7cadc3a02ec9e09534b0907b64
pedromshin <pedro.shin@afya.com.br>
2022-01-12 23:56:46 -0300

    begins header drawer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,28 @@
 import React from "react"
+import { useState } from 'react';
 
-import { container, content, logo, hamburger } from './Header.module.scss'
+
+import { container, content, logo, hamburger, button } from './Header.module.scss'
 
 import Container from '../Container/Container'
 
 export default function Header() {
 
+  const [drawerVisible, setDrawerVisible] = useState(false);
+
   return (
     <Container className={container}>
       <div className={content}>
         <img src='/logo.png' className={logo}/>
-        <img src='/hamburger.svg' className={hamburger}/>
+        <button
+              type="button"
+              className={button}
+              onClick={() => {
+                setDrawerVisible(true);
+              }}
+            >
+               <img src='/hamburger.svg' className={hamburger}/>
+            </button>
       </div>
     </Container>
   )
