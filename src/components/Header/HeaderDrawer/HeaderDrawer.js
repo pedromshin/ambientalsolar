18664d5a280c7d25d3ef0a44d5ac4dbc3bd6ed87
pedromshin <pedro.shin@afya.com.br>
2022-01-14 10:28:43 -0300

    drawer color

--- src/components/Header/HeaderDrawer/HeaderDrawer.js
+++ src/components/Header/HeaderDrawer/HeaderDrawer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createPortal } from 'react-dom';
 
 import { container, content } from './HeaderDrawer.module.scss';
 import Container from '../../Container/Container';
@@ -6,12 +7,12 @@ import Container from '../../Container/Container';
 function HeaderDrawer(props) {
   const renderDrawer = () => {
     return (
-      <Container className={container}>
+      <Container className={container} id='drawer-portal'>
         <div className={content}>content</div>
       </Container>
     );
   };
-  return <>{props.isOpen ? renderDrawer() : <></>}</>;
+  return props.isOpen ? createPortal(renderDrawer, document.getElementById('drawer-portal')) : null 
 }
 
 export default HeaderDrawer;
