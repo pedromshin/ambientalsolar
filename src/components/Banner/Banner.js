185113283164f2c488af27a6cb96c627c5cdceba
pedromshin <pedro.shin@afya.com.br>
2022-01-16 07:10:27 -0300

    adds photos and style slider

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { container, content, sliderWrapper, slide } from './Banner.module.scss';
+import { container, content, sliderWrapper, slide, slideImage } from './Banner.module.scss';
 import Container from '../Container/Container';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,6 +10,7 @@ import 'swiper/css';
 SwiperCore.use([EffectCoverflow, Pagination]);
 
 function Banner() {
+
   return (
     <Container className={container}>
       <div className={content}>
@@ -19,7 +20,7 @@ function Banner() {
           centeredSlides={true}
           slidesPerView={'auto'}
           coverflowEffect={{
-            rotate: 50,
+            rotate: 90,
             stretch: 0,
             depth: 100,
             modifier: 1,
@@ -28,19 +29,19 @@ function Banner() {
           className={sliderWrapper}
         >
           <SwiperSlide className={slide}>
-            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+            <img src="/projetosFotos/costaVerde/org_2b73319c4453a5b4_1633179868000.jpg" className={slideImage}/>
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" className={slideImage}/>
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+            <img src="https://swiperjs.com/demos/images/nature-3.jpg" className={slideImage}/>
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+            <img src="https://swiperjs.com/demos/images/nature-4.jpg" className={slideImage}/>
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+            <img src="https://swiperjs.com/demos/images/nature-5.jpg" className={slideImage}/>
           </SwiperSlide>
         </Swiper>
       </div>
