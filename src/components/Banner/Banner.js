95a4e51054364e343f639b14902cf377edb646b9
pedromshin <pedro.shin@afya.com.br>
2022-01-16 08:46:03 -0300

    title

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import { container, content, textWrapper, title, subtitle, sliderWrapper, slide, slideImage } from './Banner.module.scss';
+import {
+  container,
+  content,
+  textWrapper,
+  title,
+  underline,
+  subtitle,
+  sliderWrapper,
+  slide,
+  slideImage,
+} from './Banner.module.scss';
 import Container from '../Container/Container';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
@@ -10,18 +20,14 @@ import 'swiper/css';
 SwiperCore.use([EffectCoverflow, Pagination]);
 
 function Banner() {
-
   return (
     <Container className={container}>
       <div className={content}>
         <div className={textWrapper}>
-
-        <h2 className={title}>
-          Projetos
-        </h2>
-        <span className={subtitle}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </span>
+          <h2 className={title}>
+            Projetos <div className={underline}></div>
+          </h2>
+          <span className={subtitle}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
         </div>
         <Swiper
           effect={'coverflow'}
@@ -38,19 +44,39 @@ function Banner() {
           className={sliderWrapper}
         >
           <SwiperSlide className={slide}>
-            <img src='/projetosFotos/costaVerde/foto1.jpg' className={slideImage} alt="slideImage" />
+            <img
+              src="/projetosFotos/costaVerde/foto1.jpg"
+              className={slideImage}
+              alt="slideImage"
+            />
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src='/projetosFotos/costaVerde/foto2.jpg' className={slideImage} alt="slideImage" />
+            <img
+              src="/projetosFotos/costaVerde/foto2.jpg"
+              className={slideImage}
+              alt="slideImage"
+            />
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src='/projetosFotos/costaVerde/foto3.jpg' className={slideImage} alt="slideImage" />
+            <img
+              src="/projetosFotos/costaVerde/foto3.jpg"
+              className={slideImage}
+              alt="slideImage"
+            />
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src='/projetosFotos/costaVerde/foto4.jpg' className={slideImage} alt="slideImage" />
+            <img
+              src="/projetosFotos/costaVerde/foto4.jpg"
+              className={slideImage}
+              alt="slideImage"
+            />
           </SwiperSlide>
           <SwiperSlide className={slide}>
-            <img src='/projetosFotos/costaVerde/foto5.jpg' className={slideImage} alt="slideImage" />
+            <img
+              src="/projetosFotos/costaVerde/foto5.jpg"
+              className={slideImage}
+              alt="slideImage"
+            />
           </SwiperSlide>
         </Swiper>
       </div>
